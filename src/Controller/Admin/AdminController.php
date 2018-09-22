840997d5584b8a446d64ef57c3c8957f85100b24
Oleksandr Zakharenko <zakharenko.it@gmail.com>
2018-09-22 10:11:36 +0200

    added unique entity validation

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use App\Entity\Faq;
 use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
--- src/Entity/Faq.php
+++ src/Entity/Faq.php
@@ -3,9 +3,11 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\FaqRepository")
+ * @UniqueEntity(fields={"question"}, message="It looks like you already have a question!")
  */
 class Faq
 {
@@ -17,7 +19,7 @@ class Faq
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $question;
 
